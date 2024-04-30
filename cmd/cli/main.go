3f31f36c9a51bca847b9c65b56c0349c45894ff5
Eduardo Henrique <edu.hen.fm@gmail.com>
2024-04-30 05:55:37 -0400

    feat: add format option to CPF

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,4 +1,15 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/ed-henrique/sdg/pkg"
+)
+
 func main() {
+  cpfs := pkg.CPFWithOptions(100, pkg.Options{ Format: true });
+
+  for _, c := range cpfs {
+    fmt.Println(c)
+  }
 }
--- pkg/cpf.go
+++ pkg/cpf.go
@@ -6,6 +6,12 @@ import (
 	"github.com/bytedance/gopkg/lang/fastrand"
 )
 
+// CPFOptions hold the options to use in CPFWithOptions.
+type CPFOptions struct {
+	// Whether the output should be in the format "XXX.XXX.XXX-XX" or not.
+	Format bool
+}
+
 // verifyingDigits generates the two verifying digits (p3 and p4) for CPF,
 // as stated by Receita Federal of Brazil.
 func verifyingDigits(p1, p2 uint32) (p3, p4 uint32) {
@@ -43,7 +49,34 @@ func CPF(n int) []string {
 		p1 := fastrand.Uint32n(100000000) // Part 1: random 8 digit number
 		p2 := fastrand.Uint32n(10)        // Part 2: tax region code
 		p3, p4 := verifyingDigits(p1, p2) // Part 3: verifying digits
-		r[i] = fmt.Sprintf("%d%d%d%d", p1, p2, p3, p4)
+		r[i] = fmt.Sprintf("%08d%d%d%d", p1, p2, p3, p4)
+	}
+
+	return r
+}
+
+// CPFWithOptions generates n strings with valid CPF numbers using the given options.
+func CPFWithOptions(n int, o CPFOptions) []string {
+	r := make([]string, n)
+
+	for i := range n {
+		p1 := fastrand.Uint32n(100000000) // Part 1: random 8 digit number
+		p2 := fastrand.Uint32n(10)        // Part 2: tax region code
+		p3, p4 := verifyingDigits(p1, p2) // Part 3: verifying digits
+
+		if o.Format {
+			r[i] = fmt.Sprintf(
+				"%03d.%03d.%02d%d-%d%d",
+				(p1/100000)%1000,
+				(p1/100)%1000,
+				p1%100,
+				p2,
+				p3,
+				p4,
+			)
+		} else {
+			r[i] = fmt.Sprintf("%08d%d%d%d", p1, p2, p3, p4)
+		}
 	}
 
 	return r
