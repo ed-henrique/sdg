b469d92c0f106caa526736b1d5b840adc8239393
Eduardo Henrique <edu.hen.fm@gmail.com>
2024-05-02 10:33:22 -0400

    refactor: makes the options available through optional functions

--- pkg/cpf.go
+++ pkg/cpf.go
@@ -6,10 +6,25 @@ import (
 	"github.com/bytedance/gopkg/lang/fastrand"
 )
 
-// CPFOptions hold the options to use in CPFWithOptions.
-type CPFOptions struct {
-	// Whether the output should be in the format "XXX.XXX.XXX-XX" or not.
-	Format bool
+type config struct {
+	format *bool
+}
+
+type CPFOption interface {
+	apply(*config)
+}
+
+type option func(*config)
+func (o option) apply(c *config) {
+	o(c)
+}
+
+// Whether the output should be in the format "XXX.XXX.XXX-XX" or not.
+func WithFormat() CPFOption {
+	return option(func(o *config) {
+		format := true
+		o.format = &format
+	})
 }
 
 // verifyingDigits generates the two verifying digits (p3 and p4) for CPF,
@@ -41,43 +56,49 @@ func verifyingDigits(p1, p2 uint32) (p3, p4 uint32) {
 	return p3, p4
 }
 
-// CPF generates n strings with valid CPF numbers.
-func CPF(n int) []string {
+func generate() (p1, p2, p3, p4 uint32) {
+	p1 = fastrand.Uint32n(100000000) // Part 1: random 8 digit number
+	p2 = fastrand.Uint32n(10)        // Part 2: tax region code
+	p3, p4 = verifyingDigits(p1, p2) // Part 3: verifying digits
+	return
+}
+
+// CPF generates n strings with valid CPF numbers, supporting an option to
+// format them.
+func CPF(n int, opts ...CPFOption) []string {
+	var config config
+	for _, opt := range opts {
+		opt.apply(&config)
+	}
+
 	r := make([]string, n)
 
+	var outputCPF func(p1, p2, p3, p4 uint32) string
+	if config.format != nil && *config.format {
+		outputCPF = formattedCPF
+	} else {
+		outputCPF = unformattedCPF
+	}
+
 	for i := range n {
-		p1 := fastrand.Uint32n(100000000) // Part 1: random 8 digit number
-		p2 := fastrand.Uint32n(10)        // Part 2: tax region code
-		p3, p4 := verifyingDigits(p1, p2) // Part 3: verifying digits
-		r[i] = fmt.Sprintf("%08d%d%d%d", p1, p2, p3, p4)
+		p1, p2, p3, p4 := generate()
+		r[i] = outputCPF(p1, p2, p3, p4)
 	}
 
 	return r
 }
 
-// CPFWithOptions generates n strings with valid CPF numbers using the given options.
-func CPFWithOptions(n int, o CPFOptions) []string {
-	r := make([]string, n)
+func formattedCPF(p1, p2, p3, p4 uint32) string {
+	return fmt.Sprintf("%03d.%03d.%02d%d-%d%d",
+		(p1/100000)%1000,
+		(p1/100)%1000,
+		p1%100,
+		p2,
+		p3,
+		p4,
+	)
+}
 
-	for i := range n {
-		p1 := fastrand.Uint32n(100000000) // Part 1: random 8 digit number
-		p2 := fastrand.Uint32n(10)        // Part 2: tax region code
-		p3, p4 := verifyingDigits(p1, p2) // Part 3: verifying digits
-
-		if o.Format {
-			r[i] = fmt.Sprintf(
-				"%03d.%03d.%02d%d-%d%d",
-				(p1/100000)%1000,
-				(p1/100)%1000,
-				p1%100,
-				p2,
-				p3,
-				p4,
-			)
-		} else {
-			r[i] = fmt.Sprintf("%08d%d%d%d", p1, p2, p3, p4)
-		}
-	}
-
-	return r
+func unformattedCPF(p1, p2, p3, p4 uint32) string {
+	return fmt.Sprintf("%08d%d%d%d", p1, p2, p3, p4)
 }
